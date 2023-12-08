afbb29d7cd3a89fd0f531743d8368bec0ebd982b
Alejandro Jimenez Corea <pichicareto24@gmail.com>
2023-12-07 23:57:56 -0600

    Se agrega la funcionalidad de agregar dinero

--- examen2/src/components/MaquinaDeCafe.js
+++ examen2/src/components/MaquinaDeCafe.js
@@ -1,11 +1,11 @@
-// CoffeeMachine.js
 import React, { useState } from 'react';
 import CoffeeItem from './cafeItem';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MaquinaDeCafe = () => {
   const [tipoCafeSeleccionado, setTipoCafeSeleccionado] = useState('');
-  const [cantidadSeleccionada, setCantidadSeleccionada] = useState(1); // Se inicia con la cantidad predeterminada de 1
+  const [cantidadSeleccionada, setCantidadSeleccionada] = useState(0);
+  const [dineroIngresado, setDineroIngresado] = useState(0);
   const [cafes, setCafes] = useState([
     { tipo: 'Americano', cantidad: 10, precio: 850 },
     { tipo: 'Capuchino', cantidad: 8, precio: 950 },
@@ -13,19 +13,45 @@ const MaquinaDeCafe = () => {
     { tipo: 'Mocachino', cantidad: 15, precio: 1300 },
   ]);
 
+  const cantidadesIniciales = {
+    1000: 20,
+    500: 20,
+    100: 30,
+    50: 50,
+    25: 25,
+  };
+
+  const [cantidadMonedas, setCantidadMonedas] = useState({ ...cantidadesIniciales });
+
   const realizarCompra = () => {
     const indiceCafeSeleccionado = cafes.findIndex((cafe) => cafe.tipo === tipoCafeSeleccionado);
-
+  
     if (indiceCafeSeleccionado !== -1) {
       const cafeSeleccionado = cafes[indiceCafeSeleccionado];
-      if (cantidadSeleccionada > 0 && cantidadSeleccionada <= cafeSeleccionado.cantidad) {
-        const cafesActualizados = [...cafes];
-        cafesActualizados[indiceCafeSeleccionado].cantidad -= cantidadSeleccionada;
-        setCafes(cafesActualizados);
+      const costoTotal = cantidadSeleccionada * cafeSeleccionado.precio;
+  
+      if (dineroIngresado >= costoTotal) {
+        if (cafeSeleccionado.cantidad > 0 && cantidadSeleccionada <= cafeSeleccionado.cantidad) {
+          const cafesActualizados = [...cafes];
+          cafesActualizados[indiceCafeSeleccionado].cantidad -= cantidadSeleccionada;
+          setCafes(cafesActualizados);
+          setDineroIngresado(0);
+          setTipoCafeSeleccionado('');
+          setCantidadSeleccionada(0);
+        } else {
+          alert('Cantidad de café insuficiente');
+        }
       } else {
-        alert('Cantidad inválida');
+        alert('Dinero insuficiente');
       }
     }
+  };
+
+  const agregarDinero = (denominacion) => {
+    const dineroActualizado = { ...cantidadMonedas };
+    dineroActualizado[denominacion]++;
+    setCantidadMonedas(dineroActualizado);
+    setDineroIngresado(dineroIngresado + parseInt(denominacion));
   };
 
   const calcularCostoTotal = () => {
@@ -53,7 +79,11 @@ const MaquinaDeCafe = () => {
 
       <div className="row justify-content-center mt-4">
         <div className="col-md-6">
-          <select className="form-control" onChange={(e) => setTipoCafeSeleccionado(e.target.value)}>
+          <select
+            className="form-control"
+            value={tipoCafeSeleccionado}
+            onChange={(e) => setTipoCafeSeleccionado(e.target.value)}
+          >
             <option value="">Seleccione el tipo de café...</option>
             {cafes.map((cafe, index) => (
               <option key={index} value={cafe.tipo}>
@@ -70,9 +100,15 @@ const MaquinaDeCafe = () => {
           <input
             type="number"
             className="form-control"
-            value={cantidadSeleccionada}
+            value={tipoCafeSeleccionado === '' ? 0 : cantidadSeleccionada}
             onChange={(e) => setCantidadSeleccionada(parseInt(e.target.value))}
           />
+        </div>
+      </div>
+
+      <div className="row justify-content-center mt-2">
+        <div className="col-md-6">
+          <p>Total: {calcularCostoTotal()} colones</p>
         </div>
       </div>
 
@@ -84,9 +120,27 @@ const MaquinaDeCafe = () => {
         </div>
       </div>
 
-      <div className="row justify-content-center mt-2">
+      <div className="row justify-content-center mt-4">
         <div className="col-md-6">
-          <p>Total: {calcularCostoTotal()} colones</p>
+          <h2>Agregar Dinero</h2>
+          <p>Dinero agregado: {dineroIngresado} colones</p>
+          <div className="btn-group">
+            <button className="btn btn-secondary" onClick={() => agregarDinero('1000')}>
+              ₡1000
+            </button>
+            <button className="btn btn-secondary" onClick={() => agregarDinero('500')}>
+              ₡500
+            </button>
+            <button className="btn btn-secondary" onClick={() => agregarDinero('100')}>
+              ₡100
+            </button>
+            <button className="btn btn-secondary" onClick={() => agregarDinero('50')}>
+              ₡50
+            </button>
+            <button className="btn btn-secondary" onClick={() => agregarDinero('2')}>
+              ₡25
+            </button>
+          </div>
         </div>
       </div>
     </div>
